985f7594200a68109c8a06bd915979488e8b71b7
PNardman <philipp.nardmann@hs-osnabrueck.de>
2013-09-23 09:42:21 +0200

    SQL data

--- Dokumente/evaluation.sql
+++ Dokumente/evaluation.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 12. Sep 2013 um 18:10
+-- Erstellungszeit: 23. Sep 2013 um 09:41
 -- Server Version: 5.5.29
 -- PHP-Version: 5.4.10
 
@@ -32,43 +32,7 @@ CREATE TABLE `answertoquestion` (
   KEY `questionId` (`questionId`),
   KEY `questionnaireId` (`questionnaireId`),
   KEY `answerhash` (`answerhash`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=31 ;
-
---
--- Daten für Tabelle `answertoquestion`
---
-
-INSERT INTO `answertoquestion` (`id`, `answertext`, `questionId`, `questionnaireId`, `answernumber`, `answerhash`) VALUES
-(1, 'Hipsterstudiengang', 4, 32, NULL, 'd312654c71c8332f3435ee0102809355e78863df'),
-(2, NULL, 5, 32, 13, 'd312654c71c8332f3435ee0102809355e78863df'),
-(3, NULL, 85, 32, 2, 'd312654c71c8332f3435ee0102809355e78863df'),
-(4, NULL, 86, 32, 4, 'd312654c71c8332f3435ee0102809355e78863df'),
-(5, NULL, 87, 32, 3, 'd312654c71c8332f3435ee0102809355e78863df'),
-(6, NULL, 89, 32, 3, 'd312654c71c8332f3435ee0102809355e78863df'),
-(7, NULL, 90, 32, 4, 'd312654c71c8332f3435ee0102809355e78863df'),
-(8, NULL, 91, 32, 3, 'd312654c71c8332f3435ee0102809355e78863df'),
-(9, NULL, 92, 32, 4, 'd312654c71c8332f3435ee0102809355e78863df'),
-(10, NULL, 93, 32, 4, 'd312654c71c8332f3435ee0102809355e78863df'),
-(11, NULL, 94, 32, 8, 'd312654c71c8332f3435ee0102809355e78863df'),
-(12, NULL, 95, 32, 4, 'd312654c71c8332f3435ee0102809355e78863df'),
-(13, 'Ich ein hipster bin', 96, 32, NULL, 'd312654c71c8332f3435ee0102809355e78863df'),
-(14, 'Yolo', 97, 32, NULL, 'd312654c71c8332f3435ee0102809355e78863df'),
-(15, 'Deine mudda', 98, 32, NULL, 'd312654c71c8332f3435ee0102809355e78863df'),
-(16, 'Medieninformatik', 4, 32, NULL, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(17, NULL, 5, 32, 7, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(18, NULL, 85, 32, 5, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(19, NULL, 86, 32, 3, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(20, NULL, 87, 32, 1, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(21, NULL, 89, 32, 3, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(22, NULL, 90, 32, 2, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(23, NULL, 91, 32, 4, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(24, NULL, 92, 32, 3, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(25, NULL, 93, 32, 0, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(26, NULL, 94, 32, 5, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(27, NULL, 95, 32, 2, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(28, 'jjjjjuuuuuuuuuuusa', 96, 32, NULL, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(29, 'hahhahaha', 97, 32, NULL, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13'),
-(30, 'plutti', 98, 32, NULL, '7df99b9fd370f89629aa7e26bbb28ff5cbd10d13');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=61 ;
 
 -- --------------------------------------------------------
 
@@ -88,18 +52,7 @@ CREATE TABLE `participant` (
 --
 
 INSERT INTO `participant` (`hash`, `questionnaireId`) VALUES
-('a23595d43756eca68308112402a30394656db474', 21),
-('27a1fb282f93a832b106f85bd268644f399fe700', 22),
-('96a617ac3c8a06a47f40220726b07f22b05a4919', 22),
-('4f8c785d82ce6cddc6a25e9d8d858c3fc00514bc', 23),
-('dd98f89bf9100367956e6293a65e98a104020e62', 23),
-('01dc7215e0da8bab9583b052686377fd4a318cd6', 24),
-('3fc9ff86751934466f053ffcb99a46e469bc39a3', 24),
-('047a9d26b99a75a0e16a34022fd84f5f65fda0ee', 25),
-('e7a4bd0a2d8fac9ecd624d9dd4b48d4c28b94fc8', 26),
-('5c2099eb1e768389a36a3a7db54e5bab9321873b', 31),
-('913bf39dc06572ae6105e9107f0fdce2768ef163', 31),
-('e1b73cf6335640cbb1ada21f5ae786433a348d75', 32);
+('a17b969cee0970f33cde965b1e1971d1d33c04d2', 34);
 
 -- --------------------------------------------------------
 
@@ -118,7 +71,7 @@ CREATE TABLE `question` (
   `prio` int(3) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `category` (`category`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=99 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
 
 --
 -- Daten für Tabelle `question`
@@ -127,48 +80,48 @@ CREATE TABLE `question` (
 INSERT INTO `question` (`id`, `text`, `category`, `rangeMin`, `rangeMax`, `rangeStep`, `type`, `prio`) VALUES
 (1, 'Studiengang:', 'VL', 0, 0, 0, 'text', 100),
 (2, 'Semester:', 'VL', 1, 20, 1, 'musel', 99),
-(3, 'Ich habe X% der Vorlesungen besucht', 'VL', 0, 100, 5, 'musel', 0),
+(3, 'Ich habe X% der Vorlesungen besucht', 'VL', 0, 100, 5, 'musel', 7),
 (4, 'Studiengang:', 'PR', 0, 0, 0, 'text', 100),
 (5, 'Semester:', 'PR', 1, 20, 1, 'musel', 99),
-(59, 'Der inhaltliche Aufbau ist logisch nachvollziehbar', 'VL', 0, 0, 0, 'radio', 0),
-(60, 'Ein Bezug zwischen Theorie und Praxis/Anwendung wird hergestellt', 'VL', 0, 0, 0, 'radio', 0),
-(61, 'Die / der Lehrende spricht verständlich(Lautstärke, Deutlichkeit)', 'VL', 0, 0, 0, 'radio', 0),
-(62, 'Der / die Lehrende kann Komplizierte versändlich machen', 'VL', 0, 0, 0, 'radio', 0),
-(63, 'Die Veranstaltung hat mein Interesse an der Thematik geweckt', 'VL', 0, 0, 0, 'radio', 0),
-(64, 'Zum Mitdenken und Durchdenken des Stoffes/Themas wird angeragt', 'VL', 0, 0, 0, 'radio', 0),
-(65, 'Die / der Lehrende wirkt vorbereitet', 'VL', 0, 0, 0, 'radio', 0),
-(66, 'Die Materialien zur Vorlesung sind hilfreich (Skripte, Folien, etc.)', 'VL', 0, 0, 0, 'radio', 0),
-(67, 'Medien (Tafel, OHP, Beamer) werden sinnvoll eingesetzt', 'VL', 0, 0, 0, 'radio', 0),
-(68, 'Die / der Lehrende ist kooperativ und aufgeschlossen', 'VL', 0, 0, 0, 'radio', 0),
-(69, 'Das Tempo des Kurses ist angemessen', 'VL', 0, 0, 0, 'radio', 0),
-(70, 'Der Umfang ist angemessen', 'VL', 0, 0, 0, 'radio', 0),
-(71, 'Der Schwierigkeitsgrad ist angemessen', 'VL', 0, 0, 0, 'radio', 0),
-(72, 'Außerhalb der Veranstaltung findet eine gute Betreuung statt', 'VL', 0, 0, 0, 'radio', 0),
-(73, 'Die / der Lehrende fördert Fragen und aktive Mitarbeit', 'VL', 0, 0, 0, 'radio', 0),
-(74, 'Auf Beiträge der Teilnehmer/-innen gibt es ein hilfreiches Feedback', 'VL', 0, 0, 0, 'radio', 0),
-(75, 'Die Prüfungsanforderungen sind transparent', 'VL', 0, 0, 0, 'radio', 0),
-(76, 'Ich lerne viel in der Veranstaltung', 'VL', 0, 0, 0, 'radio', 0),
-(77, 'Die aktive Beteiligung der Studierenden bewerte ich als sehr gut', 'VL', 0, 0, 0, 'radio', 0),
-(78, 'Die Ausstattung der Hörsäle ist sehr gut', 'VL', 0, 0, 0, 'radio', 0),
-(79, 'Der Aufwand in Stunden/Woche für Vor-/Nachbereitung beträgt', 'VL', 0, 50, 1, 'musel', 0),
-(81, 'Ich gebe der Vorlesung die Gesamtnote(Noten von 1 bis 5)', 'VL', 1, 5, 0.5, 'musel', 0),
-(82, 'Ich gebe diese Note weil', 'VL', 0, 0, 0, 'tarea', 0),
-(83, 'Mir hat besonders gefallen', 'VL', 0, 0, 0, 'tarea', 0),
-(84, 'Mir hat nicht gefallen', 'VL', 0, 0, 0, 'tarea', 0),
+(59, 'Der inhaltliche Aufbau ist logisch nachvollziehbar', 'VL', 0, 0, 0, 'radio', 69),
+(60, 'Ein Bezug zwischen Theorie und Praxis/Anwendung wird hergestellt', 'VL', 0, 0, 0, 'radio', 68),
+(61, 'Die / der Lehrende spricht verständlich(Lautstärke, Deutlichkeit)', 'VL', 0, 0, 0, 'radio', 78),
+(62, 'Der / die Lehrende kann Kompliziertes versändlich machen', 'VL', 0, 0, 0, 'radio', 77),
+(63, 'Die Veranstaltung hat mein Interesse an der Thematik geweckt', 'VL', 0, 0, 0, 'radio', 9),
+(64, 'Zum Mitdenken und Durchdenken des Stoffes/Themas wird angeregt', 'VL', 0, 0, 0, 'radio', 73),
+(65, 'Die / der Lehrende wirkt vorbereitet', 'VL', 0, 0, 0, 'radio', 79),
+(66, 'Die Materialien zur Vorlesung sind hilfreich (Skripte, Folien, etc.)', 'VL', 0, 0, 0, 'radio', 59),
+(67, 'Medien (Tafel, OHP, Beamer) werden sinnvoll eingesetzt', 'VL', 0, 0, 0, 'radio', 88),
+(68, 'Die / der Lehrende ist kooperativ und aufgeschlossen', 'VL', 0, 0, 0, 'radio', 74),
+(69, 'Das Tempo des Kurses ist angemessen', 'VL', 0, 0, 0, 'radio', 67),
+(70, 'Der Umfang ist angemessen', 'VL', 0, 0, 0, 'radio', 66),
+(71, 'Der Schwierigkeitsgrad ist angemessen', 'VL', 0, 0, 0, 'radio', 65),
+(72, 'Außerhalb der Veranstaltung findet eine gute Betreuung statt', 'VL', 0, 0, 0, 'radio', 57),
+(73, 'Die / der Lehrende fördert Fragen und aktive Mitarbeit', 'VL', 0, 0, 0, 'radio', 76),
+(74, 'Auf Beiträge der Teilnehmer/-innen gibt es ein hilfreiches Feedback', 'VL', 0, 0, 0, 'radio', 75),
+(75, 'Die Prüfungsanforderungen sind transparent', 'VL', 0, 0, 0, 'radio', 64),
+(76, 'Ich lerne viel in der Veranstaltung', 'VL', 0, 0, 0, 'radio', 8),
+(77, 'Die aktive Beteiligung der Studierenden bewerte ich als sehr gut', 'VL', 0, 0, 0, 'radio', 72),
+(78, 'Die Ausstattung der Hörsäle ist sehr gut', 'VL', 0, 0, 0, 'radio', 89),
+(79, 'Der Aufwand in Stunden/Woche für Vor-/Nachbereitung beträgt', 'VL', 0, 50, 1, 'musel', 56),
+(81, 'Ich gebe der Vorlesung die Gesamtnote(Noten von 1 bis 5)', 'VL', 1, 5, 0.5, 'musel', 3),
+(82, 'Ich gebe diese Note weil', 'VL', 0, 0, 0, 'tarea', 2),
+(83, 'Mir hat besonders gefallen', 'VL', 0, 0, 0, 'tarea', 5),
+(84, 'Mir hat nicht gefallen', 'VL', 0, 0, 0, 'tarea', 4),
 (85, 'Die Laborausstattung ist sehr gut', 'PR', 0, 0, 0, 'radio', 0),
 (86, 'Die Unterlagen zur Vorbereitung sind hilfreich und verständlich', 'PR', 0, 0, 0, 'radio', 0),
 (87, 'Die Anforderungen sind transparent', 'PR', 0, 0, 0, 'radio', 0),
-(88, 'Der Bezug zur Vorlesung ist gegeben', 'VL', 0, 0, 0, 'radio', 0),
+(88, 'Der Bezug zur Vorlesung ist gegeben', 'PR', 0, 0, 0, 'radio', 0),
 (89, 'Die Betreuenden gehen aufgeschlossen auf meine Fragen ein', 'PR', 0, 0, 0, 'radio', 0),
 (90, 'Die Verfügbarkeit der Betreuenden ist gegeben', 'PR', 0, 0, 0, 'radio', 0),
 (91, 'Der Umfang der Versuche/Übungen ist angemessen', 'PR', 0, 0, 0, 'radio', 0),
-(92, 'Die selbstständige Durchführung der Versuche/Übungen wird gefördert', 'PR', 0, 0, 0, 'radio', 0),
 (93, 'Das Feedback ist hilfreich', 'PR', 0, 0, 0, 'radio', 0),
 (94, 'Der Aufwand für Vor- und Nachbereitung in Stunden beträgt insgesamt', 'PR', 0, 50, 1, 'musel', 0),
 (95, 'Ich gebe dem Praktikum die Gesamtnote', 'PR', 1, 5, 0.5, 'musel', 0),
 (96, 'Ich gebe diese Note weil', 'PR', 0, 0, 0, 'tarea', 0),
 (97, 'Mir hat besonders gefallen', 'PR', 0, 0, 0, 'tarea', 0),
-(98, 'Mir hat nicht gefallen', 'PR', 0, 0, 0, 'tarea', 0);
+(98, 'Mir hat nicht gefallen', 'PR', 0, 0, 0, 'tarea', 0),
+(99, 'Der / die Lehrende motiviert die Teilehmenden zum Selbststudium', 'VL', 0, 0, 0, 'radio', 58);
 
 -- --------------------------------------------------------
 
@@ -186,21 +139,14 @@ CREATE TABLE `questionnaire` (
   PRIMARY KEY (`id`),
   KEY `category` (`category`),
   KEY `profId` (`profId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=36 ;
 
 --
 -- Daten für Tabelle `questionnaire`
 --
 
 INSERT INTO `questionnaire` (`id`, `profId`, `expirationDate`, `semester`, `courseName`, `category`) VALUES
-(21, 3, '2013-09-13', 'SoSe 2013', 'ARMA', 'PR'),
-(22, 3, '2013-09-13', 'SoSe 2013', 'Huibuh', 'PR'),
-(23, 3, '2013-09-13', 'SoSe 2013', 'ARMA', 'VL'),
-(24, 3, '2013-09-13', 'SoSe 2013', 'RMA', 'VL'),
-(25, 5, '2013-09-13', 'SoSe 2013', 'ARMA', 'PR'),
-(26, 5, '2013-09-13', 'SoSe 2013', 'AppleFanboyism', 'PR'),
-(31, 3, '2013-09-13', 'SoSe 2013', 'Test mit GEorg ', 'PR'),
-(32, 5, '2013-09-11', 'SoSe 2013', 'Finaler Test', 'PR');
+(34, 7, '2013-09-30', 'WiSe 2013/2014', 'TestModul', 'VL');
 
 -- --------------------------------------------------------
 
@@ -214,14 +160,6 @@ CREATE TABLE `submitters` (
   PRIMARY KEY (`answerhash`),
   KEY `questionnaireId` (`questionnaireId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `submitters`
---
-
-INSERT INTO `submitters` (`answerhash`, `questionnaireId`) VALUES
-('7df99b9fd370f89629aa7e26bbb28ff5cbd10d13', 32),
-('d312654c71c8332f3435ee0102809355e78863df', 32);
 
 -- --------------------------------------------------------
 
@@ -237,17 +175,15 @@ CREATE TABLE `user` (
   `fullName` varchar(128) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fullName` (`fullName`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
 
 --
 -- Daten für Tabelle `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `role`, `fullName`) VALUES
-(3, 'admin', 'admin', 'admin', 'Herr Prof. Von Und Zu'),
-(4, 'user', 'user', 'user', 'Studiensekretariat'),
-(5, 'plutti', 'apple', 'admin', 'Björn Plutka'),
-(6, 'juergi', 'mathe', 'admin', 'Dr. Jürgen Kampmann');
+(7, 'testprof', 'test', 'admin', 'Prof. Dr.  Max Mustertest'),
+(8, 'secretary', 'secretary', 'user', 'Studierendensekretariat');
 
 --
 -- Constraints der exportierten Tabellen
